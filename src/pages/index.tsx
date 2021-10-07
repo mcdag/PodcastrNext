fc7b6cea910a9f54a8044be4585c93e551392fbf
mcdag <mcdag@cin.ufpe,br>
2021-10-07 12:02:41 -0300

    Adição de uma static generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
-export default function Home() {
+import { GetStaticProps } from 'next';
+
+type Episode = {
+
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
-   <h1>Index</h1>
+  <div>
+    <h1>Index</h1>
+    <p>{JSON.stringify(props.episodes)}</p>
+  </div>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60* 60 * 8,
+  }
 }
