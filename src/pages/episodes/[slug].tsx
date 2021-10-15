e0317bf35a51425d95a3fb21e78709eab33f6b11
mcdag <mcdag@cin.ufpe,br>
2021-10-14 23:27:52 -0300

    Adição dos titulos de home - episode no título da página

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,6 +1,7 @@
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import Image from 'next/image';
+import Head from 'next/head';
 import Link from 'next/link';
 import { GetStaticPaths, GetStaticProps } from "next";
 import { api } from '../../services/api';
@@ -30,6 +31,10 @@ export default function Episode({episode }: EpisodeProps){
 
   return(
     <div className={styles.episode}>
+      <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
+
       <div className={styles.thumbnailContainer}>
         <Link href='/'>
           <button type='button' onClick={() => play(episode)}>
